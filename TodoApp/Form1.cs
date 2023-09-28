f22ba11b9b524c049e64bd21c6962fd78f8472ee
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-09-28 18:06:25 +0100

    Updated Search button

--- TodoApp/Form1.cs
+++ TodoApp/Form1.cs
@@ -19,11 +19,6 @@ namespace TodoApp
 
         }
 
-        //private void AddButton_Click(object sender, EventArgs e)
-        //{
-        //    AddTodoItem();
-        //}
-
         private void AddButton_Click_1(object sender, EventArgs e)
         {
             AddTodoItem();
@@ -78,36 +73,6 @@ namespace TodoApp
 
         }
 
-        //private void EditButton_Click(object sender, EventArgs e)
-        //{
-        //    using (var dbContext = new TodoAppDBContext())
-        //    {
-        //        if (GridView1.SelectedRows.Count > 0)
-        //        {
-        //            selectedTaskId = Convert.ToInt32(GridView1.SelectedRows[0].Cells["ID"].Value);
-        //            string existingName = GridView1.SelectedRows[0].Cells["Name"].Value.ToString();
-        //            string existingTask = GridView1.SelectedRows[0].Cells["Task"].Value.ToString();
-        //            string existingDescription = GridView1.SelectedRows[0].Cells["Description"].Value.ToString();
-        //            DateTime existingDateCreated = (DateTime)(GridView1.SelectedRows[0].Cells["DateCreated"].Value);
-        //            DateTime existingDueDate = (DateTime)(GridView1.SelectedRows[0].Cells["DueDate"].Value);
-
-
-        //            NameTextBox.Text = existingName;
-        //            TaskTextBox.Text = existingTask;
-        //            DescriptionTextBox.Text = existingDescription;
-        //            DateCreated.Value = existingDateCreated;
-        //            DueDate.Value = existingDueDate;
-        //            dbContext.SaveChanges();
-        //        }
-        //        else
-        //        {
-        //            MessageBox.Show("Please select a task to edit");
-        //        }
-        //    }
-
-        //}
-
-
         private void EditButton_Click_1(object sender, EventArgs e)
         {
             using (var dbContext = new TodoAppDBContext())
@@ -136,26 +101,6 @@ namespace TodoApp
             }
         }
 
-        //private void LoadButton_Click(object sender, EventArgs e)
-        //{
-
-        //    // Call your _toDoListService (or data access layer) to fetch all to-do items
-        //    List<TodoItem> todoItems = _toDoListService.GetAllTodoItems();
-
-        //    if (todoItems != null && todoItems.Count > 0)
-        //    {
-        //        // Set the DataGridView's data source to the retrieved to-do items
-        //        GridView1.DataSource = todoItems;
-        //    }
-        //    else
-        //    {
-        //        // If there are no to-do items, you can display a message or handle it as needed
-        //        MessageBox.Show("No to-do items found.");
-        //    }
-
-
-        //}
-
         private void LoadButton_Click_1(object sender, EventArgs e)
         {
             // Call your _toDoListService (or data access layer) to fetch all to-do items
@@ -173,7 +118,7 @@ namespace TodoApp
             }
         }
 
-        //private void SearchButton_Click(object sender, EventArgs e)
+        //private void SearchTodo_Click(object sender, EventArgs e)
         //{
         //    try
         //    {
@@ -205,16 +150,15 @@ namespace TodoApp
         //    {
         //        MessageBox.Show("Error" + ex);
         //    }
-
         //}
-
 
         private void SearchTodo_Click(object sender, EventArgs e)
         {
             try
             {
                 // Get the search criteria from the user input (e.g., a search term or date range)
-                string searchTerm = SearchBox.Text.Trim();
+                string searchTerm = SearchBox.Text.Trim().ToLower(); // Convert search term to lowercase for case-insensitive search
+
                 // Ensure that at least one search criterion is provided
                 if (string.IsNullOrEmpty(searchTerm))
                 {
@@ -224,62 +168,29 @@ namespace TodoApp
 
                 // Perform the search based on the criteria
                 List<TodoItem> allTodoItems = _toDoListService.GetAllTodoItems();
-                var filteredItem = allTodoItems.Where(x => x.Task.ToLower().Contains(searchTerm) ||
-                x.Name.ToLower().Contains(searchTerm) || x.Description.ToLower().Contains(searchTerm)).ToList();
-                if (filteredItem.Any())
+
+                var filteredItems = allTodoItems.Where(item =>
+                    item.Name.ToLower().Contains(searchTerm) || // Search by Name
+                    item.Task.ToLower().Contains(searchTerm) || // Search by Task
+                    item.DateCreated.ToString("yyyy-MM-dd").Contains(searchTerm) // Search by Date Created (formatted as yyyy-MM-dd)
+                ).ToList();
+
+                if (filteredItems.Any())
                 {
-                    GridView1.DataSource = filteredItem;
+                    GridView1.DataSource = filteredItems;
                     MessageBox.Show("Search result loaded successfully");
                 }
                 else
                 {
                     GridView1.DataSource = null;
-                    MessageBox.Show("search keyword not found");
+                    MessageBox.Show("No items matching the search keyword found");
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error" + ex);
+                MessageBox.Show("Error: " + ex.Message);
             }
         }
-
-        //private void DeleteButton_Click(object sender, EventArgs e)
-        //{
-        //    try
-        //    {
-
-        //        if (GridView1.SelectedRows.Count == 0)
-        //        {
-        //            MessageBox.Show("Please select one or more tasks to delete");
-        //            return;
-        //        }
-
-        //        DialogResult result = MessageBox.Show("Are you sure you want to delete the selected task(s) ?", "Confirm delete", MessageBoxButtons.YesNo);
-
-        //        if (result == DialogResult.Yes)
-        //        {
-        //            List<int> selectedTaskIds = new List<int>();
-        //            foreach (DataGridViewRow row in GridView1.SelectedRows)
-        //            {
-        //                int taskIdToDelete = Convert.ToInt32(row.Cells["ID"].Value);
-        //                selectedTaskIds.Add(taskIdToDelete);
-        //            }
-
-        //            foreach (int itemId in selectedTaskIds)
-        //            {
-        //                _toDoListService.DeleteTodoItem(itemId);
-        //            }
-        //        }
-
-        //        RefreshDataGridView();
-
-        //        Reset();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show("Error" + ex);
-        //    }
-        //}
 
 
         private void DeleteButton_Click_1(object sender, EventArgs e)
@@ -319,48 +230,6 @@ namespace TodoApp
                 MessageBox.Show("Error" + ex);
             }
         }
-
-
-        //private void UpdateButton_Click(object sender, EventArgs e)
-        //{
-        //    try
-        //    {
-        //        if (selectedTaskId == 0)
-        //        {
-        //            MessageBox.Show("Please select a to-do item to update.");
-        //            return;
-        //        }
-
-        //        // Find the existing to-do item by its ID (selectedTaskId)
-        //        TodoItem existingItem = _toDoListService.GetTodoItemById(selectedTaskId);
-
-        //        if (existingItem == null)
-        //        {
-        //            MessageBox.Show("Selected to-do item not found.");
-        //            return;
-        //        }
-
-        //        // Update the properties of the existing item with new values
-        //        existingItem.Name = NameTextBox.Text;
-        //        existingItem.Task = TaskTextBox.Text;
-        //        existingItem.Description = DescriptionTextBox.Text;
-        //        existingItem.DateCreated = DateCreated.Value;
-        //        existingItem.DueDate = DueDate.Value;
-
-        //        // Save or apply the changes to your data source
-        //        _toDoListService.UpdateTodoItem(existingItem);
-
-        //        // Refresh your DataGridView to reflect the changes
-        //        RefreshDataGridView();
-
-        //        // Reset the form or clear input fields
-        //        Reset();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show("Error" + ex);
-        //    }
-        //}
 
         private void UpdateButton_Click_1(object sender, EventArgs e)
         {
